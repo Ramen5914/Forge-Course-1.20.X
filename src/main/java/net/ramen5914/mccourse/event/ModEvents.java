693db26971012682cfa9457a819cf3b058f17d4a
Ramen5914 <Ramen5914@gmail.com>
2024-03-15 07:45:10 -0700

    Update ModEvents.java

--- src/main/java/net/ramen5914/mccourse/event/ModEvents.java
+++ src/main/java/net/ramen5914/mccourse/event/ModEvents.java
@@ -145,28 +145,4 @@ public class ModEvents {
                 new ItemStack(ModItems.KOHLRABI_SEEDS.get()), 3, 2, 0.02f
         ));
     }
-
-    @SubscribeEvent
-    public static void registerCapabilities(RegisterCapabilitiesEvent event) {
-        event.registerBlockEntity(
-                Capabilities.ItemHandler.BLOCK,
-                ModBlockEntities.GEM_EMPOWERING_STATION_BE_TYPE.get(),
-                (be, side) -> new ItemStackHandler(4) {
-                    @Override
-                    protected void onContentsChanged(int slot) {
-                        be.setChanged();
-                    }
-
-                    @Override
-                    public boolean isItemValid(int slot, @NotNull ItemStack stack) {
-                        return switch (slot) {
-                            case 0, 1 -> true;
-                            case 2 -> false;
-                            case 3 -> stack.getItem() == ModItems.KOHLRABI.get();
-                            default -> super.isItemValid(slot, stack);
-                        };
-                    }
-                }
-        );
-    }
 }
